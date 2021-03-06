1967bb7f4c983e168fe8721b36dc3285ba54f2cf
Malik Edwards <malikedwards777@gmail.com>
2021-03-06 10:41:29 -0500

    Exercises 6-8 Completed

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -31,10 +31,13 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        username = form.username.data
+        password = form.username.data
+
+        if username and password:
             # Get the username and password values from the form.
 
             # using your model, query database for a user based on the username
@@ -42,12 +45,25 @@ def login():
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
+            user = UserProfile.query.filter_by(username=username).first()
 
-            # get user id, load into session
-            login_user(user)
+            if user is not None and check_password_hash(user.password, password):
+                remember_me  = False
 
-            # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+                if 'remember_me' in request.form:
+                    remember_me = True
+
+                # get user id, load into session
+                login_user(user, remember=remember_me)
+
+                # remember to flash a message to the user
+                flash('Logged in successfully.', 'success')
+
+                return redirect(url_for("/secure-page"))  # they should be redirected to a secure-page route instead
+            else:
+                flash('Username or Password is incorrect.','danger')
+    
+    flash_errors(form)
     return render_template("login.html", form=form)
 
 
@@ -56,6 +72,16 @@ def login():
 @login_manager.user_loader
 def load_user(id):
     return UserProfile.query.get(int(id))
+
+
+def flash_errors(form):
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash(u"Error in the %s field - %s" % (
+                getattr(form, field).label.text,
+                error
+            ), 'danger')
+
 
 ###
 # The functions below should be applicable to all Flask apps.
